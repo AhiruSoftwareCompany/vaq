8f698a9d52b3ca8dd5c97acd784b9e2714a8c2f6
Leander Dreier <leletec@mail.de>
2018-12-18 17:03:06 +0100

    Also send the current rating to the client, #1

--- server/DataAccess.php
+++ server/DataAccess.php
@@ -22,17 +22,28 @@ class DataAccess {
     public function getRandomQuote() {
         $entries = explode("---\n", file_get_contents($this->quotesPath));
         $index = rand(0, count($entries) - 1); // Get a random index in the range of entries
-        $data = preg_split("#\n\s*\n#Uis", $entries[$index]); // Separates headers from body
+        $data = preg_split("#\n\s*\n#Uis", $entries[$index]); // Separate headers from body
         $headers = $this->http_parse_headers($data[0]); // also exists in pecl, but not a standard php function
         $body = substr($data[1], 0, -1); // Remove the last \n from the body
-        $quote = new Quote($json = false, $headers['id'], $headers['date'], $body);
+
+        $rating = 0;
+        $lines = file($this->ratingsPath, FILE_IGNORE_NEW_LINES);
+        foreach ($lines as $i => $line) {
+            $values = explode(' ', $line);
+            if ($values[0] === $headers['id']) {
+                $rating = $values[1];
+                break;
+            }
+        }
+
+        $quote = new Quote($json = false, $headers['id'], $headers['date'], $body, $rating);
         return $quote;
     }
 
     public function refreshRating($data) {
         $quote = new Quote($json = $data);
         $id = $quote->getId();
-        if ($id == -1) return 400;
+        if ($id == -1) return 400; // the id -1 is considered invalid
         $rating = $quote->getRating();
 
         $found = false;
@@ -41,7 +52,7 @@ class DataAccess {
             $values = explode(' ', $line);
             if ($values[0] === $id) {
                 if ($found) { // Check if we already found and changed an entry
-                    $lines[$i] = "-1 " . $line; // mark line as invalid
+                    $lines[$i] = "-1 " . $line; // Mark line as invalid
                 } else {
                     $found = true;
                     $values[1] += $rating; // might change to absolute rating
