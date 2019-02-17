AhiruSoftwareCompany/vaq

commit 5b431604ffc470de87a94604aec12569b2cdd7bc

Fix bug where last quote was not displayable

quotelistParser/parse.php
@@ -22,12 +22,25 @@ foreach ($tops as $i => $top) {
 
     $subs = preg_split('/\n*-{10,}/', $top); // Split Top into the multi and single segment
 
+    // Single-line-quotes
+    $startId = ((7 + $i) * 10 + 1) * 10000 + 1;
+    shell_exec("echo \"$subs[1]\" | awk -v i=$startId -f single.awk >> $out");
+
     // Multi-line-quotes
+    $first = true;
     $startId = (7 + $i) * 100000 + 1;
     $quotes = explode(PHP_EOL.PHP_EOL, $subs[0]);
     foreach($quotes as $k => $quote) {
         if ($k < 1) continue; // Skip headline of Top
-        $output = "id: $startId\n";
+        $output = '';
+
+        if (!$first) {
+            $output .= "---\n";
+        } else {
+            $first = false;
+        }
+
+        $output .= "id: $startId\n";
         $lines = explode(PHP_EOL, $quote); // Split quote into separate lines
 
         foreach($lines as $l => $line) {
@@ -47,12 +60,7 @@ foreach ($tops as $i => $top) {
             }
         }
 
-        $output .= "---\n";
         file_put_contents($out, $output, FILE_APPEND);
         $startId++;
     }
-
-    // Single-line-quotes
-    $startId = ((7 + $i) * 10 + 1) * 10000 + 1;
-    shell_exec("echo \"$subs[1]\" | awk -v i=$startId -f single.awk >> $out");
 }

commit b21145f8406f99f9b1bc34328895db69abcd0b86

Fix bug introduced in 5b43160

quotelistParser/parse.php
@@ -17,9 +17,15 @@ $file = fopen($out, "w+") or die("Can't write output file!\n");
 fclose($file);
 if (!file_exists($in)) die("Can't read input file!\n");
 $tops = preg_split('/\n*={10,}/', file_get_contents($in)); // Split file into different Tops
+$firstTop = true;
 
 foreach ($tops as $i => $top) {
     if ($i < 3) continue; // Skip intro, Top 0 and Top 1
+
+    if (!$firstTop)
+        file_put_contents($out, "---\n", FILE_APPEND);
+    else
+        $firstTop = false;
 
     $lines = file($originsPath, FILE_IGNORE_NEW_LINES);
     $origin = $lines[$i-3];
@@ -31,18 +37,17 @@ foreach ($tops as $i => $top) {
     shell_exec("echo \"$subs[1]\" | awk -v start=$startId -v origin=\"$origin\" -f single.awk >> $out");
 
     // Multi-line-quotes
-    $first = true;
+    $firstMLQuote = true;
     $startId = (7 + $i) * 100000 + 1;
     $quotes = explode(PHP_EOL.PHP_EOL, $subs[0]);
     foreach($quotes as $k => $quote) {
         if ($k < 1) continue; // Skip headline of Top
         $output = '';
 
-        if (!$first) {
+        if (!$firstMLQuote)
             $output .= "---\n";
-        } else {
-            $first = false;
-        }
+        else
+            $firstMLQuote = false;
 
         $output .= "id: $startId\n";
         $lines = explode(PHP_EOL, $quote); // Split quote into separate lines